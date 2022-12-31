ea77dbcf4465851b334d55ab4a6433f95db2e91c
THT <thehelpfultipper@gmail.com>
2022-12-30 20:16:41 -0800

    Adding holiday greet codepen source code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# holiday_greetings
+# Responsive Flex Layout
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/thehelpfultipper/pen/wvxMZWM](https://codepen.io/thehelpfultipper/pen/wvxMZWM).
+
---
+++ src/script.js
@@ -0,0 +1,30 @@
+const mssg = document.querySelector('#mssg'),
+      text = 'Happy Holidays! Best of health and happiness :) — Klea ❤ —',
+      speed = 50;
+
+// Dynamic typewriting 
+let i = 0; 
+function write() {
+  if ( i < text.length ) {
+    let letter = text.charAt(i);
+    
+    mssg.innerHTML += letter;
+    
+    if ( letter === '!' || letter === ')' ) {
+      mssg.innerHTML += '<br />';
+    }
+    
+    i++;
+    
+    setTimeout(write, speed);
+  }
+} 
+
+write();
+
+// Display action item for animation after typing ends
+let animate = setTimeout( () => {
+  document.querySelector('#lottie').innerHTML = '<lottie-player src="https://thehelpfultipper.github.io/holiday_greetings/new_year_greet.json"  background="transparent"  speed="1" loop  autoplay></lottie-player>';
+  
+}, text.length * 52);
+
